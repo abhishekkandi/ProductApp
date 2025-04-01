0935266d86a2168f2ba90a7a84ec0cd5e94fde06
Abhishek Kandi <kandii.abhishek@gmail.com>
2025-04-01 23:01:12 +0530

    Added security headers

--- ProductApp.API/Extensions/WebAppExtensions.cs
+++ ProductApp.API/Extensions/WebAppExtensions.cs
@@ -5,6 +5,9 @@ namespace ProductApp.API.Extensions
     {
         public static void ConfigureApp(this WebApplication app)
         {
+
+            app.UseCors("AllowAll");
+
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
@@ -13,12 +16,20 @@ namespace ProductApp.API.Extensions
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Product API v1"));
             }
             
+            app.Use(async (context, next) =>
+            {
+                context.Response.Headers.Add("Strict-Transport-Security", "max-age=31536000; includeSubDomains; preload");
+                context.Response.Headers.Add("Cache-Control", "no-store, no-cache, must-revalidate");
+                await next();
+            });
+
             app.UseHttpsRedirection();
             
-            app.UseRouting();
-            
-            app.UseCors("AllowAll");
-            
+            app.UseRouting(); 
+            app.UseXContentTypeOptions();//Setting X-Content-Type-Options header
+            app.UseReferrerPolicy(opts => opts.NoReferrer()); //Referrer-Policy header
+            app.UseXXssProtection(options => options.EnabledWithBlockMode());//X-XSS-Proetection header
+            app.UseXfo(options => options.Deny()); //X-Frame-Options head
             app.UseAuthentication();
             app.UseAuthorization();
             
